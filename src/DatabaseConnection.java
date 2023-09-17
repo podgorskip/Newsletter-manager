713d7e5f61f3de4fc230902ece191a3f7d2867dc
Patryk Podgórski <podgorski.p@interia.eu>
2023-09-17 13:59:19 +0200

    Implements class to handle database connection

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,2 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    public static Connection connection;
+
+    static {
+        String url = "jdbc:mysql://localhost:3306/newsletter";
+        String user = "root";
+        String password = "MySQL.password";
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DatabaseConnection() {}
 }
