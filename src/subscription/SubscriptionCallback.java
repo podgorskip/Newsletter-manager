7a04ec1b67c40d55b8079d80fa2860f7a5c66ddb
Patryk Podgórski <podgorski.p@interia.eu>
2023-09-19 13:21:13 +0200

    Implements interface provding callbacks handling

--- src/subscription/SubscriptionCallback.java
+++ src/subscription/SubscriptionCallback.java
@@ -1,5 +1,15 @@
 package subscription;
 
+/**
+ * An interface that allows to handle data provided by a user.
+ */
 public interface SubscriptionCallback {
+    /**
+     * Notifies and passes provided data after a user clicks the confirmation button.
+     * @param firstName provided first name
+     * @param lastName provided last name
+     * @param medium provided medium (email/phone number)
+     * @param type provided subscription type
+     */
     void onSubscriptionConfirmed(String firstName, String lastName, String medium, Subscriber.SubscriptionType type);
 }
